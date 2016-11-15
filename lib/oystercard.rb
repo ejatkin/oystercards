338f17cce9ebe645eb34a6a52f842b07dc0a8063
unicornelia <ez.kornelia@gmail.com>
2016-11-15 18:10:08 +0000

    Added maximum balance to card

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,16 @@
 class Oystercard
 
-  attr_reader :money, :balance
+  attr_reader :balance
 
-  def initialize(money=0)
-    @money = money
-    @balance
+  def initialize
+    @balance = 0
   end
 
-  def top_up(value)
+  def top_up(amount)
+    raise 'The card has reached its maximum, no more top up' if balance == 90
+    @balance += amount
   end
+
+
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,16 +2,38 @@ require 'oystercard'
 
 describe Oystercard do
 
-  it 'has money on the card, its initial value is 0' do
-    expect(subject.money).to eq 0
+  context 'balance on card' do
+
+    it 'has money on the card, its initial value is 0' do
+      expect(subject.balance).to eq 0
+    end
+
+    it 'responds to the balance method' do
+      expect(subject).to respond_to(:balance)
+    end
   end
 
-  it 'has a balance on the card' do
-    expect(subject).to respond_to(:balance)
+  context '#top_up' do
+
+    it 'can do topping up' do
+      expect(subject).to respond_to(:top_up).with(1).argument
+    end
+
+    it 'the top up increases the amount on the card' do
+      amount = 10
+      expect(subject.top_up(amount)).to eq(subject.balance)
+    end
+
+    context 'maximum balance' do
+
+      it 'raise error if we try to top of after the card reached the maximum' do
+        subject.top_up(90)
+        expect{subject.top_up(1)}.to raise_error('The card has reached its maximum, no more top up')
+      end
+
+    end
   end
 
-  it 'can do topping up' do
-    expect(subject).to respond_to(:top_up).with(1).argument
-  end
+
 
 end
