b8575e5be8f0c09176a94ba2ef17a1c98720a2f2
Agata Sumowska <agata.sumowska@gmail.com>
2016-11-16 12:15:56 +0000

    Adds status for in_journey

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,9 +1,10 @@
 class Oystercard
 
-  attr_reader :balance
+  attr_reader :balance, :in_journey
 
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(amount)
@@ -15,4 +16,13 @@ class Oystercard
     @balance -= amount
   end
 
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    message = "You can only touch out if you already touched in" 
+    raise message if @in_journey == false
+    @in_journey = false
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,52 +2,81 @@ require 'oystercard'
 
 describe Oystercard do
 
+  subject(:oyster) {described_class.new}
+
+
   context 'balance on card' do
 
     it 'has money on the card, its initial value is 0' do
-      expect(subject.balance).to eq 0
+      expect(oyster.balance).to eq 0
     end
 
     it 'responds to the balance method' do
-      expect(subject).to respond_to(:balance)
+      expect(oyster).to respond_to(:balance)
     end
   end
 
   context '#top_up' do
 
     it 'can do topping up' do
-      expect(subject).to respond_to(:top_up).with(1).argument
+      expect(oyster).to respond_to(:top_up).with(1).argument
     end
 
     it 'the top up increases the amount on the card' do
       amount = 10
-      expect(subject.top_up(amount)).to eq(subject.balance)
+      expect(oyster.top_up(amount)).to eq(oyster.balance)
     end
 
     context 'maximum balance' do
 
       it 'raise error if we try to top of after the card reached the maximum' do
-        subject.top_up(90)
-        expect{subject.top_up(1)}.to raise_error('The card has reached its maximum, no more top up')
+        oyster.top_up(90)
+        expect{oyster.top_up(1)}.to raise_error('The card has reached its maximum, no more top up')
       end
 
     end
   end
 
-  context '#deduct_money' do
+  context 'deduct_money' do
 
     before(:each) do
-      subject.top_up(70)
+      oyster.top_up(70)
       @amount = 1
     end
 
     it 'should respond to the deduct method' do
-      expect(subject).to respond_to(:deduct).with(@amount).argument
+      expect(oyster).to respond_to(:deduct).with(@amount).argument
     end
 
     it 'should deduct money from the card and return new balance' do
-      expect{subject.deduct(@amount)}.to change{subject.balance}.by(-@amount)
+      expect{oyster.deduct(@amount)}.to change{oyster.balance}.by(-@amount)
     end
+  end
+
+  context 'When checking the satus' do
+    it "should return the status of the card" do
+      expect(oyster.in_journey).to eq false
+    end
+
+    it "should change the status is true" do
+      oyster.touch_in
+      expect(oyster.in_journey).to eq true
+    end
+
+    it "should change the status is true" do
+      oyster.touch_in
+      oyster.touch_out
+      expect(oyster.in_journey).to eq false
+    end
+    
+  end
+  context "raise error" do
+
+    it "when trying to touch out without touching in" do
+      message = "You can only touch out if you already touched in"
+      expect{oyster.touch_out}.to raise_error message
+    end
+
   end
 
 end
